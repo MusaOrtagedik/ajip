c5cc1d0fb46e67d882689050d6c596608c59d3f4
MusaOrtagedik <33340440+MusaOrtagedik@users.noreply.github.com>
2017-11-03 09:01:52 +0300

    Add files via upload

---
+++ build-ajip-Desktop-Debug/moc_ajip.cpp
@@ -0,0 +1,106 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ajip.h'
+**
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.7)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../ajip/ajip.h"
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ajip.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 63
+#error "This file was generated using the moc from 4.8.7. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+static const uint qt_meta_data_ajip[] = {
+
+ // content:
+       6,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: signature, parameters, type, tag, flags
+       6,    5,    5,    5, 0x08,
+      24,    5,    5,    5, 0x08,
+      44,    5,    5,    5, 0x08,
+      64,    5,    5,    5, 0x08,
+      84,    5,    5,    5, 0x08,
+     106,    5,    5,    5, 0x08,
+
+       0        // eod
+};
+
+static const char qt_meta_stringdata_ajip[] = {
+    "ajip\0\0readStandardOut()\0readStandardError()\0"
+    "on_btnSec_clicked()\0on_btnKur_clicked()\0"
+    "on_btnKapat_clicked()\0"
+    "on_btnEklentiYukle_clicked()\0"
+};
+
+void ajip::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        ajip *_t = static_cast<ajip *>(_o);
+        switch (_id) {
+        case 0: _t->readStandardOut(); break;
+        case 1: _t->readStandardError(); break;
+        case 2: _t->on_btnSec_clicked(); break;
+        case 3: _t->on_btnKur_clicked(); break;
+        case 4: _t->on_btnKapat_clicked(); break;
+        case 5: _t->on_btnEklentiYukle_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObjectExtraData ajip::staticMetaObjectExtraData = {
+    0,  qt_static_metacall 
+};
+
+const QMetaObject ajip::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_ajip,
+      qt_meta_data_ajip, &staticMetaObjectExtraData }
+};
+
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &ajip::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
+const QMetaObject *ajip::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
+}
+
+void *ajip::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_ajip))
+        return static_cast<void*>(const_cast< ajip*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ajip::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
